4f6efef867a477bb1e42051388ac4e77fe329175
alisonsunn <alison.jyue.sun@gmail.com>
2024-04-22 17:06:07 +1000

    add ellipases

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,8 @@ export const Hero = () => {
 
   return (
     <section className={styles.container}>
+      <img id={styles.Ellipse1} src={getURL('Ellipse 1.png')} alt="" />
+      <img id={styles.Ellipse2} src={getURL('Ellipse 2.png')} alt="" />
       <div className={styles.content}>
         <h1>Hi, I'm Alison</h1>
         <p>I’m a full-stack developer with 5 years
